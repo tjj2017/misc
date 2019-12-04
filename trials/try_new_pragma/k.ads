742f89bfaf58156632767bd6c7ed37ac2d908be5
Trevor Jennings <TrevorJennings@bcs.org>
2019-12-04 19:23:13 +0000

    Update test file

--- trials/try_new_pragma/k.ads
+++ trials/try_new_pragma/k.ads
@@ -3,14 +3,15 @@ is
    subtype Co_Ord is Integer range 0 .. 360;
 
    To_Be_Updated : Integer
-   with Annotate => (ASVAT, Represents, "Try_New_Pragma.To_Be_Updated");
+     with Annotate => (ASVAT, Represents, "Try_New_Pragma.To_Be_Updated");
    X : Co_Ord;
    Y : Co_Ord;
 
-   pragma Import (Ada,
-                  Entity     => To_Be_Updated,
-                  External_Name => "Represents",
-                  Link_Name => "Try_New_Pragma.To_Be_Updated");
+
+--     pragma Import (Ada,
+--                    Entity     => To_Be_Updated,
+--                    External_Name => "Represents",
+--                    Link_Name => "Try_New_Pragma.To_Be_Updated");
    pragma Import (Ada, X, "Represents",
                   "Try_New_Pragma.X:Try_New_Pragma.Co_Ord");
    pragma Import (Ada, Y, "Represents",
@@ -18,9 +19,9 @@ is
 
    procedure Update
      with Global => (In_Out => (X, Y, To_Be_Updated)),
-         Annotate => ASVAT, --  (ASVAT, Nondet_In_Type),
-         Import => True,
-         External_Name => "Nondet";
+     Annotate => (ASVAT, Nondet_In_Type),
+     Import => True;
+--           External_Name => "Nondet";
 --     Convention => Ada;
 
 --   pragma Global (In_Out => (X, Y, To_Be_Updated));
