64de16b03cf3db7a2cae691aad0eb6ff5cb0ba85
Trevor Jennings <TrevorJennings@bcs.org>
2020-02-28 10:13:14 +0000

    Update test

--- trials/copy_to_address/model/try_mem_copy.adb
+++ trials/copy_to_address/model/try_mem_copy.adb
@@ -1,7 +1,16 @@
-with Mem_Copy;
+with Buffers; use type Buffers.Smaller_Buf;
+--  with Mem_Copy;
 procedure Try_Mem_Copy is
-   A : Integer := 23;
-   B : Integer := 0;
+--     A : Integer := 23;
+--     B : Integer := 0;
+   Local_B, Remote_B : Buffers.Smaller_Buf := (0, (others => 0));
 begin
-   Mem_Copy (B'Address, A'Address, A'Size);
+   Remote_B := Local_B;
+   pragma Assert (Local_B = Remote_B);
+   Local_B.Size := 1;
+   Local_B.Data (1) := 1;
+   pragma Assert (Local_B = Remote_B);
+   Remote_B.Size := 1;
+   Remote_B.Data (1) := 1;
+   pragma Assert (Local_B = Remote_B);
 end Try_Mem_Copy;
