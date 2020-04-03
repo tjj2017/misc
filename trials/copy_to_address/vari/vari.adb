0ad5b1ac340012fb1eab12bb9a72abb9b00fb7d3
Trevor Jennings <TrevorJennings@bcs.org>
2020-04-03 09:38:02 +0100

    Transfer to Mac

--- trials/copy_to_address/vari/vari.adb
+++ trials/copy_to_address/vari/vari.adb
@@ -1,24 +1,28 @@
-with Text_IO; use Text_IO;
+with System;
 procedure Vari is
    subtype Count is Integer range 0 .. 10;
-   subtype Index is Count range Count'First .. Count'last - 1;
-   type AF is array (Index) of Integer;
-   type F is record
-      C : Count;
-      D : AF;
-   end record;
+--     subtype Index is Count range Count'First .. Count'last - 1;
+--     type AF is array (Index) of Integer;
+--     type F is record
+--        C : Count;
+--        D : AF;
+--     end record;
+--
+--     type AV is array (Index range  <>) of Integer;
+--     type V (N : Index) is record
+--        C : Count;
+--        D : AV (Count'First .. N);
+--     end record;
+--
+--     subtype VF is V (Count'Last - 1);
+--
+--     V_F : F := (0, (others => 0));
+--     V_V : VF := (Count'Last - 1, 0, (others => 0));
+   procedure With_Address (A : System.Address)
+     with Annotate => (ASVAT, Memcpy),
+     Import;
 
-   type AV is array (Index range  <>) of Integer;
-   type V (N : Index) is record
-      C : Count;
-      D : AV (Count'First .. N);
-   end record;
-
-   subtype VF is V (Count'Last - 1);
-
-   V_F : F := (0, (others => 0));
-   V_V : VF := (Count'Last - 1, 0, (others => 0));
+   C : Count := 5;
 begin
-   Put_Line ("V_F'Size = " & Integer'Image (V_F'Size));
-   Put_Line ("V_V'Size = " & Integer'Image (V_V'Size));
+   With_Address (C'Address);
 end Vari;
