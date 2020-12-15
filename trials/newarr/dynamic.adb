51c28822e0164c6bc9485b90d061e78962f8a9df
Trevor Jennings <TrevorJennings@bcs.org>
2020-12-15 12:32:09 +0000

    Having to commit to pull.

--- trials/newarr/dynamic.adb
+++ trials/newarr/dynamic.adb
@@ -1,13 +1,15 @@
 procedure Dynamic is
+   type Arr_U is array (Integer range <>) of Integer;
    Low : Integer;
    High : Integer;
 begin
    Low := 1;
    High := 10;
    declare
-      type A is array (Low .. High) of Integer;
+      subtype A is Arr_U (Low .. High);
       VA : A;
    begin
       VA (1) := 10;
+      pragma Assert (VA (1) = 10);
    end;
 end Dynamic;
