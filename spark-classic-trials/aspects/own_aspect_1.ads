55ddae96c0d4f66a83e9182eb4c6450d74045726
Trevor Jennings <TrevorJennings@bcs.org>
2022-01-05 18:51:33 +0000

    Update Tests and spec.

--- spark-classic-trials/aspects/own_aspect_1.ads
+++ spark-classic-trials/aspects/own_aspect_1.ads
@@ -1,7 +1,8 @@
+with P;
 package Own_Aspect_1
 --  1 --# own V;
-with Classic_Own => ((V))
---  2 --# own in V;
+with Classic_Own => (((Plain => V)))
+--  --  2 --# own in V;
 --  with Classic_Own => (((External_In => V)))
 --  3 --# own out V;
 --  with Classic_Own => (((External_Out => V)))
@@ -14,31 +15,46 @@ with Classic_Own => ((V))
 --  7 --# own protected out V;
 --  with Classic_Own => (((Protected_Out => V)))
 --  8 --# own V, PV;
---  with Classic_Own => ((V, PV))
+--  with Classic_Own => (((Plain => V, Plain => PV)))
 --  9 --# own V, PV, State;
---  with Classic_Own => ((V, PV, State))
+--  with Classic_Own => (((Plain => V, Plain => PV, Plain => State)))
 --  10 --# own in IP, V, PV, out OP, State, protected in PI;
 --  with Classic_Own =>
---    (((External_In => V), PV, (External_Out => OP), State, (Protected_In => PI)))
+--    (((External_In => V, Plain => PV, External_Out => OP,
+--       Plain => State, Protected_In => PI)))
 --  11 --# own V : Integer;
---  with Classic_Own => (((V) with Own_Type => Integer))
+--  with Classic_Own =>(((Plain => V) with Own_Type => Integer))
 --  12 --# own V : Abstract_Type;
---  with Classic_Own => (((V) with Own_Type => Abstract_Type))
+--  with Classic_Own => (((Plain => V) with Own_Type => Abstract_Type))
 --  13 --# own V, PV : Abstract_Type;
---  with Classic_Own => (((V, PV) with Own_Type => Abstract_Type))
---  14 --# own V, PV : Abstract_Type (integrity => 4);
---  with Classic_Own => (((V, PV) with Own_Type => Abstract_Type, Integrity => 4))
+--  with Classic_Own =>
+--    (((Plain => V, Plain => PV) with Own_Type => Abstact_Type))
+--  14 --# own V, PV : Abstract_Type (integrity => 4));
+--  with Classic_Own =>
+--    (((Plain => V, Plain => PV) with Own_Type => Abstract_Type, Integrity => 4))
 --  15 --# own V, PV : Abstract_Type (Integrity => 4, suspendable);
 --  with Classic_Own =>
---    (((V, PV) with Own_Type => Abstract_Type, Integrity => 4, Suspendable => True))
+--    (((Plain => V, Plain => PV) with Own_Type => Abstract_Type,
+--     Integrity => 4, Suspendable => True))
 --  16 --# own in EI, V, out OP, state, PV, protected in PI : Abstract_Type
 --             (Integrity => 4, suspendable);
 --  with Classic_Own =>
---    (((External_In => EI), V, (External_Out => OP), state, PV, (Protected_In => PI))
---     with Own_Type => Abstract_Type, Integrity => 4, Suspendable => True)
---  17 --# own V, PV : Integer, State : Abstract_Type;
---  with Classic_Own => (((V, PV) with Own_Type => Integer),
---                        ((State) with Own_Type => Abstract_Type))
+--    (((External_In => EI, Plain => V, Exteranl_Out => OP,
+--       Plain => State, Plain => PV, Protected_In => PI) with
+--     Own_Type => Abstract_Type, Integrity => 4, Suspendable => True))
+--  17 --# own V, PV : Integer; State : Abstract_Type;
+--  with Classic_Own => (((Plain => V, Plain => PV) with Own_Type => Integer),
+--                       ((Plain => State) with Own_Type => Abstract_Type))
+--  18 --# own V, PV : Integer;
+--     --# State : Abstract_Type (Integrity => 4, Suspendable => True) ;
+--  with Classic_Own => (((Plain => V, Plain => PV) with Own_Type => Integer),
+--                       ((Plain => State) with Own_Type => Abstract_Type,
+--                        Integrity => 4, Suspendable => True))
+--  19 --# own V; PV; in IP;
+--  with Classic_Own => (((Plain => V), (Plain => PV)), (External_In => IP))
+--  20 --# own V, State; PV in IP;
+--  with Classic_Own =>
+--    (((Plain => V, Plain => State), (Plain => PV, External_In => IP)))
 is
    V : Integer;
 private
