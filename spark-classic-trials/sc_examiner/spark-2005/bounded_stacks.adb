70d9a17333e8c440bbfc7d361c7938bbcca6cc8e
Trevor Jennings <TrevorJennings@bcs.org>
2025-10-16 18:57:16 +0100

    Transfer to hub.

--- spark-classic-trials/sc_examiner/spark-2005/bounded_stacks.adb
+++ spark-classic-trials/sc_examiner/spark-2005/bounded_stacks.adb
@@ -4,7 +4,9 @@ package body Bounded_Stacks is
    -- Count --
    -----------
 
-   function Count (S : Stack) return Stack_Count is
+   function Count (S : Stack) return Stack_Count
+   --# return S.Count;
+   is
    begin
       return S.Count;
    end Count;
@@ -14,12 +16,27 @@ package body Bounded_Stacks is
    -- Is_Empty --
    --------------
 
-   function Is_Empty (S : Stack) return Boolean is
+   function Is_Empty (S : Stack) return Boolean
+   --# return E => (E <-> S.Count = 0);
+   is
    begin
       return S.Count = 0;
    end Is_Empty;
 
-   procedure New_Stack (S : out Stack)
+  ---------
+   -- Top --
+   ---------
+
+   function Top (S : Stack) return Element_Type
+   --# pre (Is_Empty (S) <-> S.Count = 0) and S.Count > 0;
+   --# return T => T = S.Contents (S.Count);
+   is
+   begin
+      return S.Contents (S.Count);
+   end Top;
+   pragma Inline (Top);
+
+    procedure New_Stack (S : out Stack)
   is
    --# hide New_Stack;
    begin
@@ -33,10 +50,17 @@ package body Bounded_Stacks is
    ----------
 
    procedure Push (S : in out Stack; Value : Element_Type)
+   --# pre Count (S) = S.Count and S.Count < Stack_Count'Last;
+   --# post S.Count > 0 and not Is_Empty (S);
    is
    begin
+      --# check Is_Empty (S) <-> S.Count = 0;
       S.Count := S.Count + 1;
-      S.Contents (S.Count) := Value;
+      --# check Is_Empty (S) <-> S.Count = 0;
+      --# check (not Is_Empty (S)) <-> S.Count > 0;
+      --# check S.Count > 0;
+      --# check not Is_Empty (S);
+     S.Contents (S.Count) := Value;
    end Push;
    pragma Inline (Push);
 
@@ -45,23 +69,16 @@ package body Bounded_Stacks is
    ---------
 
    procedure Pop (S : in out Stack; Value : out Element_Type)
+   --# pre (Is_Empty (S) <-> S.Count = 0) and S.Count > 0 and
+   --#     Count (S) = S.Count;
+   --# post Count (S) = S~.Count - 1 and
+   --#      Value =  Top (S~);
    is
    begin
-      Value := S.Contents (S.Count);
+      Value := Top (S);
       S.Count := S.Count - 1;
-   end Pop;
+     end Pop;
    pragma Inline (Pop);
-
-   ---------
-   -- Top --
-   ---------
-
-   function Top (S : Stack) return Element_Type
-   is
-   begin
-      return S.Contents (S.Count);
-   end Top;
-   pragma Inline (Top);
 
    -----------------
    -- Predecessor --
