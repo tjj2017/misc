889a167b86caedc753be3a2aad2ce357c9445c1a
Trevor Jennings <trevorjennings@bcs.org>
2025-11-12 12:55:00 +0000

    Update Bounded_Stacks to use Predecessor instead of abstract stack.

--- spark-classic-trials/sc_examiner/spark-2014/proof/bounded_stacks.adb
+++ spark-classic-trials/sc_examiner/spark-2014/proof/bounded_stacks.adb
@@ -1,97 +1,75 @@
 package body Bounded_Stacks
 with SPARK_Mode
 is
-   ---------------
-   -- Stack_Abs --
-   ---------------
-
-   function Stack_Abs (S : Stack) return Stack_Abstraction is
-     ((Contents => Contents_Abstraction (S.Contents), Stack_Top => S.Count));
-
-   -----------
+    -----------
    -- Count --
    -----------
 
    function Count (S : Stack) return Stack_Count is (S.Count);
-   pragma Inline (Count);
-
-   procedure New_Stack
-     (S : out Stack)
-     with SPARK_Mode => Off
-  is
-   begin
-      --  Setting Count to Zero initialises the stack S
-      S.Count := 0;
-   end New_Stack;
-   pragma Inline (New_Stack);
-
-   ----------
-   -- Push --
-   ----------
-
-   procedure Push
-     (S : in out Stack;
-      Value : Element_Type)
-   is
-   begin
-      S.Count := S.Count + 1;
-      S.Contents (S.Count) := Value;
-   end Push;
-   pragma Inline (Push);
-
-   ---------
-   -- Pop --
-   ---------
-
-   procedure Pop
-     (S : in out Stack;
-      Value : out Element_Type)
-   is
-   begin
-      Value := S.Contents (S.Count);
-      S.Count := S.Count - 1;
-   end Pop;
-   pragma Inline (Pop);
-
-   ---------
-   -- Top --
-   ---------
-
-   function Top
-     (S : Stack)
-      return Element_Type
-   is (S.Contents (S.Count));
-   --  begin
-   --     return S.Contents (S.Count);
-   --  end Top;
-   pragma Inline (Top);
 
    -----------------
    -- Predecessor --
    -----------------
 
-   function Predecessor
-     (S : Stack;
-      Pred_Num : Stack_Count)
-      return Element_Type
+   function Predecessor (S : Stack; Pred_Num : Stack_Count)
+                         return Element_Type with
+     Refined_Post=> Predecessor'Result = S.Contents (S.Count - Pred_Num)
    is
    begin
       return S.Contents (S.Count - Pred_Num);
    end Predecessor;
-   pragma Inline (Predecessor);
+
+   ---------
+   -- Top --
+   ---------
+
+   function Top (S : Stack) return Element_Type is (S.Contents (S.Count));
+
+
+   ---------------
+   -- New_Stack --
+   ---------------
+
+   procedure New_Stack (S : out Stack) with
+     SPARK_Mode => Off
+  is
+   begin
+      --  Setting Count to Zero initialises the stack S
+      S.Count := 0;
+   end New_Stack;
+
+   ----------
+   -- Push --
+   ----------
+
+   procedure Push (S : in out Stack; Value : Element_Type)
+   is
+   begin
+      S.Count := S.Count + 1;
+      S.Contents (S.Count) := Value;
+   end Push;
+
+   ---------
+   -- Pop --
+   ---------
+
+   procedure Pop (S : in out Stack; Value : out Element_Type)
+   is
+   begin
+      Value := S.Contents (S.Count);
+      S.Count := S.Count - 1;
+   end Pop;
 
    -----------
    -- Clear --
    -----------
 
-   procedure Clear
-     (S : out Stack)
-     with SPARK_Mode => Off
+   procedure Clear (S : out Stack) with
+     SPARK_Mode => Off
    is
    begin
       --  Setting Count to zero means no contents.
       S.Count := 0;
    end Clear;
-   pragma Inline (Clear);
 
 end Bounded_Stacks;
--- spark-classic-trials/sc_examiner/spark-2014/proof/bounded_stacks.ads
+++ spark-classic-trials/sc_examiner/spark-2014/proof/bounded_stacks.ads
@@ -8,70 +8,51 @@ is
    type Stack is private;
 
    subtype Stack_Count is Natural range 0 .. Stack_Size;
-   subtype Stack_Index is natural range 1 .. Stack_Size;
-
-   type Contents_Abstraction is array (Stack_Index) of Element_Type with
-     Ghost;
-
-   type Stack_Abstraction is
-      record
-         Contents  : Contents_Abstraction;
-         Stack_Top : Stack_Count;
-      end record with
-     Ghost;
-
-   function Stack_Abs (S : Stack) return Stack_Abstraction with
-     Ghost;
 
    function Count    (S : Stack) return Stack_Count with
-     Post => Count'Result = Stack_Abs (S).Stack_Top;
+   Inline;
 
    function Is_Empty (S : Stack) return Boolean is (Count (S) = 0) with
-   Post => Is_Empty'Result = (Stack_Abs (S).Stack_Top = 0);
-   pragma Inline (Is_Empty);
-
-   procedure New_Stack (S : out Stack)
-     with Post => Is_Empty (S);
-
-   pragma Unevaluated_Use_Of_Old (Allow);
-
-   procedure Push (S : in out Stack;
-                   Value : Element_Type) with
-     Pre => Count (S) < Stack_Size,
-     Post => not Is_Empty (S) and
-             Count (S) = Count (S)'Old + 1 and
-             Top (S) = Value and
-             Stack_Abs (S).Stack_Top = Stack_Abs (S'Old).Stack_Top + 1 and
-             Stack_Abs (S).Contents (Stack_Abs (S).Stack_Top) = Value and
-             (for all P in Stack_Index range 1 .. Count (S)'Old =>
-                Stack_Abs (S)'Old.Contents (P)  = Stack_Abs (S).Contents (P));
-
-   procedure Pop  (S : in out Stack;
-                   Value : out Element_Type) with
-     Pre  => not Is_Empty (S),
-     Post => Count (S) = Count (S)'Old - 1 and
-             Value = Top (S'Old) and
-             Stack_Abs (S).Stack_Top = (Stack_Abs (S'Old).Stack_Top - 1) and
-             Value = Stack_Abs (S'Old).Contents (Stack_Abs (S'Old).Stack_Top)
-             and
-             (for all P in Stack_Index range 1 .. Count (S) =>
-                Stack_Abs (S)'Old.Contents (P)  = Stack_Abs (S).Contents (P));
-
-
-   function Top (S : Stack) return Element_Type with
-     Pre  => not Is_Empty (S),
-     Post => Top'Result = Stack_Abs (S).Contents (Stack_Abs (S).Stack_Top);
+   Inline;
 
    function Predecessor (S : Stack;
                          Pred_Num : Stack_Count)
                          return Element_Type with
      Pre  => Pred_Num < Count (S),
-     Post => Predecessor'Result = Stack_Abs (S).Contents (Pred_Num);
+     Inline;
+
+   function Top (S : Stack) return Element_Type with
+     Pre  => not Is_Empty (S),
+     Post => Top'Result = Predecessor (S, 0),
+     Inline;
+
+   procedure New_Stack (S : out Stack) with
+   Post => Is_Empty (S);
+
+   procedure Push (S : in out Stack; Value : Element_Type) with
+     Pre => Count (S) < Stack_Size,
+     Post => not Is_Empty (S) and
+             Count (S) = Count (S'Old) + 1 and
+             Top (S) = Value and
+             (for all P in Stack_Count range 0 .. Count (S'Old) =>
+                  Predecessor (S, P) = Predecessor (S'Old, P + 1)),
+     Inline;
+
+   procedure Pop  (S : in out Stack; Value : out Element_Type) with
+     Pre  => not Is_Empty (S),
+     Post => Count (S) = Count (S'Old) - 1 and
+             Value = Top (S'Old) and
+             (for all P in Stack_Count range 1 .. Count (S) =>
+                  Predecessor (S, P - 1)  = Predecessor (S'Old, P)),
+       Inline;
+
 
    procedure Clear (S : out Stack) with
-     Post => Is_Empty (S) and Stack_Abs (S).Stack_Top = 0;
+     Post => Is_Empty (S);
 
 private
+   subtype Stack_Index is Stack_Count range 1 .. Stack_Count'Last;
+
    type Stack_Contents is array (Stack_Index) of Element_Type;
 
    type Stack is
